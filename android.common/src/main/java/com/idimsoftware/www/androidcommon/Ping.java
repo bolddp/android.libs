ff1e53136fba3191af4c17ae2a12269b786cb1de
bolddp <daniel.persson@kapsch.net>
2015-11-09 23:25:24 +0100

    Created Android Studio project

---
+++ android.common/src/main/java/com/idimsoftware/www/androidcommon/Ping.java
@@ -0,0 +1,12 @@
+package com.idimsoftware.www.androidcommon;
+
+/**
+ * Created by Daniel on 2015-11-09.
+ */
+public class Ping {
+
+    public String sayHello(){
+        return "Tjena!";
+    }
+
+}
